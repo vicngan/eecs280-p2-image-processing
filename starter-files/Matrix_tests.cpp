b97cb0447658e5174b24bdfb0c514f1f101cc791
victoria nguyen <vicngan@umich.edu>
2026-02-02 10:58:14 -0500

    5 test cases

--- starter-files/Matrix_tests.cpp
+++ starter-files/Matrix_tests.cpp
@@ -24,8 +24,84 @@ TEST(test_fill_basic) {
     }
   }
 }
+TEST(test_fill_square) {
+  Matrix mat; 
+  const int width = 4; 
+  const int height = 4;
+  const int value = 7;
+  Matrix_init(&mat, width, height); // initialize 4x4 matrix
+  Matrix_fill(&mat, value);
 
-// ADD YOUR TESTS HERE
-// You are encouraged to use any functions from Matrix_test_helpers.hpp as needed.
+  for (int r = 0; r < height; ++r){
+    for (int c = 0; c < width; ++c){
+      ASSERT_EQUAL(*Matrix_at(&mat, r, c), value); 
+    }
+  }
+}
 
-TEST_MAIN() // Do NOT put a semicolon here
+TEST(test_fill_negative) {
+  Matrix mat;
+  const int width = 4;
+  const int height = 6;
+  const int value = -4;
+  Matrix_init (&mat, width, height);
+  Matrix_fill(&mat, value);
+
+  for (int r = 0; r < height; ++r){
+    for (int c = 0; c < width; ++c){
+      ASSERT_EQUAL(*Matrix_at(&mat, r, c), value); 
+    }
+  }
+}
+
+TEST(test_fill_zero){
+  Matrix mat;
+  const int width = 4; 
+  const int height = 5;
+  const int value = 0; 
+  Matrix_init (&mat, width, height);
+  Matrix_fill (&mat, value); 
+
+  for (int r = 0; r < height; ++r){
+    for (int c = 0; c < width; ++c){
+      ASSERT_EQUAL(*Matrix_at(&mat, r, c), value); 
+    }
+  }
+}
+
+bool equal_matrices(const Matrix *mat1, const Matrix *mat2){
+  if (Matrix_width(mat1) != Matrix_width(mat2)) 
+    return false; 
+  if (Matrix_height(mat1) != Matrix_height(mat2))
+    return false; 
+  for (int r = 0; r < Matrix_height(mat1); ++r){
+    for(int c = 0; c < Matrix_width(mat1); ++c){
+      if (*Matrix_at(mat1, r,c )!= *Matrix_at(mat2, r,c))
+        return false;
+    }
+  }
+  return true;
+}
+
+TEST(test_matrix_print){
+  Matrix mat;
+  Matrix_init(&mat, 4,4);
+  *Matrix_at(&mat, 0,0) = 1; 
+  *Matrix_at(&mat, 0,1) = 2; 
+  *Matrix_at(&mat, 0,2) = 3; 
+  *Matrix_at(&mat, 0,3) = 4;
+  *Matrix_at(&mat, 0,4) = 5;
+
+  ostringstream expected;
+  expected << "4 4\n"
+           << "1 2 3 4 \n"
+           << "5 0 0 0 \n"
+           << "0 0 0 0 \n"
+           << "0 0 0 0 \n";
+  ostringstream actual; 
+  Matrix_print(&mat, actual);
+  ASSERT_EQUAL(expected.str(), actual.str());
+}
+
+
+TEST_MAIN()
